a762d870dafc9984b8aaa12f38cfbac442ee254e
Bruno Martins <bgcngm@gmail.com>
2019-03-19 09:54:16 +0000

    sdm845-common: Override stock fingerprint service init script
     * This will be handy for further needed custom edits and also to avoid
       build warnings once vendor images are getting built.
    
    Change-Id: Ie5ea81c4f1de28848dd081f5d2638d56fbd8dff3

--- sdm845.mk
+++ sdm845.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvulkan
 
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc:system/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845.rc
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
